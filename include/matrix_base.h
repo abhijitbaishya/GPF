ba0bbfec30fa85594191bfa7da319796b34e531d
Smit <smit.new1@gmail.com>
2013-06-09 12:21:28 -0800

    Added the operator functions

--- include/matrix_base.h
+++ include/matrix_base.h
@@ -2,15 +2,24 @@
 #define _BASE_MAT
 
 /*
-   This is the base class from which we will derive our matrix classes
+   This is the abstract base class from which we will derive our matrix classes
 */
 class matrix_base
 {
+private:
+	int**     	buffer;	//buffer to store the matrix 
+						//(array of array can be used to store sparse matrix also)
+	int			degree;	//to store the degree of the matrix
 public:
    //This matrix class will be used to represent graphs and since graphs always use
    //squire matrices, there fore only degree is sufficient
    matrix_base(unsigned int degree);
 
+public:
+	//operators are implemented by the derived classes
+   virtual matrix_base operator+(matrix_base& operand) = 0;
+   virtual matrix_base operator-(matrix_base& operand) = 0;
+   virtual matrix_base operator*(matrix_base& operand) = 0;
 }
 
 
