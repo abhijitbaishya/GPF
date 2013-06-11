4c74c2ff16af61924977dfbdc7fed46e57b8ad3a
Smit <smit.new1@gmail.com>
2013-06-10 21:26:37 -0800

    Converted the matrix_base to pure interface.

--- include/matrix_base.h
+++ include/matrix_base.h
@@ -1,5 +1,6 @@
 #ifndef _BASE_MAT
 #define _BASE_MAT
+#include<iostream>
 
 //Graph Processor Framework
 namespace gpf
@@ -10,39 +11,55 @@ namespace gpf
 */
 class matrix_row
 {
-private:
-	int*			row_ptr;			//actual row pointer
-	unsigned int 	num_elements;		//number of elements
-public:
-	virtual ~matrix_row();					//The destructor
-	matrix_row(unsigned int num_elements);	//The constructor
-
-	matrix_row operator[](int suffix);	//returns an element from the matrix_row given an index
+	private:
+		//member variables
+		int*			row_ptr;			//actual row pointer
+		unsigned int 	num_elements;		//number of elements
+	
+	
+	public:
+		//constructors and destructors
+		virtual ~matrix_row		();								//The destructor
+		matrix_row				(unsigned int num_elements);	//The constructor
+	
+	
+	public:
+		//member functions
+		int  operator[]	(int suffix);		//returns an element from the matrix_row given an index
 										//it also handles bound checking
-}
+		unsigned int get_num_elements();	//returns no of elements in this row (may be usefull for sparse matrix)
+};
+
+
+
+
 
 /*
-   DESC : 	matrix_base is the base class for both simple_matrix and sparse_matrix to 
+   DESC : 	matrix_base is the interface for both simple_matrix and sparse_matrix to 
 			model directed and undirected graphs respectively. simple_matrix and sparse_matrix
-			are declared in there respective headers named as their class names. It encapsulates the common
-			functionalities of both types of matrices.
+			are declared in there respective headers named as their class names.
+			
+			Since it is an interface, it does not have an implementation.
 */
 class matrix_base
 {
-protected:
-	matrix_row*	matrix_rows;	//Rows of the matrix
-						
-	int			degree;	//to store the degree of the matrix
-public:
-	//to free up the buffer
-	virtual ~matrix_base();
-	
-	unsigned int 	get_degree();		//returns the degree of the matrix(both simple and sparse matrix has a degree)
-	matrix_row*		get_rows();			//returns base address of the rows array
-public:
-	virtual matrix_row operator[](int suffix) = 0;	//Returns a matrix row which also overloads a [] operator
+	public:
+		virtual unsigned int 	get_degree		() = 0;				//returns the degree of the matrix(both simple and sparse matrix has a degree)
+		virtual matrix_row*		get_rows		() = 0;				//returns base address of the rows array
+		virtual matrix_row 		operator[]		(int suffix) = 0;	//Returns a matrix row which also overloads a [] operator
 };
 
 
 }	//end of namespace
 #endif
+
+
+
+
+
+
+
+
+
+
+
